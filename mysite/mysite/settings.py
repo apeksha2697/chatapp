66d81cecadb98f21cbce3e5f8d8ec92ec70892ab
Apeksha Singh <apekshasingh664@gmail.com>
2020-08-21 14:14:29 +0530

    ASGI application sdded to settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
+ASGI_APPLICATION = 'mysite.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
